8f65f20bae79ee8d89ea45a2e99c8e67753a46ac
B--B <dark.schneider.ds@gmail.com>
2015-05-01 19:37:43 +0200

    Sepolicy: Added sepolicy from cm and fixed the build

---
+++ sepolicy/sepolicy.mk
@@ -0,0 +1,38 @@
+#
+# This policy configuration will be used by all products that
+# inherit from CM
+#
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/AOSP/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file.te \
+    file_contexts \
+    genfs_contexts \
+    property_contexts \
+    seapp_contexts \
+    service_contexts \
+    adbd.te \
+    app.te \
+    auditd.te \
+    bootanim.te \
+    drmserver.te \
+    healthd.te \
+    hostapd.te \
+    installd.te \
+    mediaserver.te \
+    netd.te \
+    property.te \
+    shell.te \
+    su.te \
+    sysinit.te \
+    system.te \
+    system_app.te \
+    system_server.te \
+    ueventd.te \
+    uncrypt.te \
+    userinit.te \
+    vold.te \
+    zygote.te \
+    mac_permissions.xml
