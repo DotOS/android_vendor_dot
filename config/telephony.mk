21f680e5b1fce7da7123268fe5670b21789bb7dc
B--B <dark.schneider.ds@gmail.com>
2015-05-10 13:00:16 +0200

    Added world apn list

---
+++ config/telephony.mk
@@ -0,0 +1,7 @@
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/AOSP/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Selective SPN list for operator number who has the problem.
+PRODUCT_COPY_FILES += \
+    vendor/AOSP/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
