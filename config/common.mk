1152e8f4212ae03f63d0adf3a99fa9379f6656e8
B--B <dark.schneider.ds@gmail.com>
2015-05-12 19:07:47 +0200

    vendor: various updates
    Added camera effects
    Enabled additional ffmpeg plugins
    
    Big thanks to CM

---
+++ config/common.mk
@@ -0,0 +1,25 @@
+PRODUCT_BRAND ?= alu-team
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.com.android.dataroaming=false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
+
+# Bring in camera effects
+PRODUCT_COPY_FILES +=  \
+    vendor/AOSP/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/AOSP/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
+
+# Stagefright FFMPEG plugin
+PRODUCT_PACKAGES += \
+    libstagefright_soft_ffmpegadec \
+    libstagefright_soft_ffmpegvdec \
+    libFFmpegExtractor \
+    libnamparser
+
+# by default, do not update the recovery with system updates
+PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
---
+++ config/common_full_phone.mk
@@ -0,0 +1,4 @@
+# Inherit common AOSP stuff
+$(call inherit-product, vendor/AOSP/config/common.mk)
+
+$(call inherit-product, vendor/AOSP/config/telephony.mk)
