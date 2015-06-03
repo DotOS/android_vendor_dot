f12c34dc7697be8a12727715376fec7f39f24f4d
B--B <dark.schneider.ds@gmail.com>
2015-06-03 19:50:30 +0200

    Moved custom init in vendor repo Added init.d support Added our banner

--- config/common.mk
+++ config/common.mk
@@ -4,8 +4,12 @@ PRODUCT_BRAND ?= JDCTeam
 PRODUCT_COPY_FILES += \
     vendor/AOSP/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
 
+
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false
@@ -15,11 +19,29 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/AOSP/overlay/common
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
 
+# init.d support
+PRODUCT_COPY_FILES += \
+    vendor/AOSP/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/AOSP/prebuilt/common/bin/sysinit:system/bin/sysinit
+
+# JDC-specific init file
+PRODUCT_COPY_FILES += \
+    vendor/AOSP/prebuilt/common/etc/init.local.rc:root/init.jdc.rc
+
+    
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \
     vendor/AOSP/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
     vendor/AOSP/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
 
+# Enable SIP+VoIP on all targets
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Enable wireless Xbox 360 controller support
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
+    
 # Stagefright FFMPEG plugin
 PRODUCT_PACKAGES += \
     libstagefright_soft_ffmpegadec \
