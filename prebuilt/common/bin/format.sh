e5ac062506ead76235ef246100db58e97a95db4b
Mattia D'Alleva <antaresone@antaresone.eu>
2015-07-19 20:11:55 +0200

    common: update format script
    - Fixed MultiROM environment recognition
    - Fixed a bug where if current filesystem is impossible to be recognized, /system is formatted as F2FS instead of EXT4 as default.
    - Updated script body, bye-bye to the ugly one used since November 2014
    - Updated strings & description
    - Whitespace fixes
    
    Change-Id: Id62d84092576d5f1c071601f453d3859fd708fa3
    
    Conflicts:
    	prebuilt/common/bin/format.sh

--- prebuilt/common/bin/format.sh
+++ prebuilt/common/bin/format.sh
@@ -1,47 +1,53 @@
-#!/sbin/busybox sh
-#    ____  ____  __   ____  ____       
-#   (_  _)( ___)(  ) (_  _)( ___)      
-#  .-_)(   )__)  )(__  )(   )__)       
-#  \____) (__)  (____)(__) (____)      
-#   ____  ____  _  _  ___  _____  _  _ 
+#!/sbin/sh
+#    ____  ____  __   ____  ____
+#   (_  _)( ___)(  ) (_  _)( ___)
+#  .-_)(   )__)  )(__  )(   )__)
+#  \____) (__)  (____)(__) (____)
+#   ____  ____  _  _  ___  _____  _  _
 #  (  _ \( ___)( \/ )/ __)(  _  )( \( )
-#   )(_) ))__)  \  /( (__  )(_)(  )  ( 
+#   )(_) ))__)  \  /( (__  )(_)(  )  (
 #  (____/(____)  \/  \___)(_____)(_)\_)
 #
 # Copyright (c) 2015 - Mattia "AntaresOne" D'Alleva
-# Copyright (c) 2015 - Jflte Dev Connection
+# Copyright (c) 2015 - Jflte Dev Connection (JDCTeam)
 #
-# Export busybox path
-export BB=/sbin/busybox
-# Check ROM installation/upgrade:
-# if touching primary ROM run filesystem check and format on /system, else don't run it
-busybox mount /system
-export MROM=ls /tmp/META-INF/com/google/android | grep "updater-script"
-busybox umount /system
-if [ "$MROM" == "" ]; then
-    echo "Installing ROM on /system"
-    # Mount /system
-    $BB mount /system
-    # Copy mounts in a text file
-    $BB mount > /tmp/mount.txt
-    # Unmount /system
-    $BB umount /system
-    # Set a value for FS
-    export FS=$(eval $(/sbin/blkid /dev/block/mmcblk0p16 | /sbin/busybox cut -c 24-); /sbin/busybox echo $TYPE > /tmp/type.txt);
-    export TYPE=`cat /tmp/type.txt | grep "ext4"`
-    # Filesystem check & format
+# EXT4/F2FS format script with MultiROM support
+# Check current /system FS and format accordingly to the FS found
+# Check installation/upgrade type. If primary ROM run format process, else don't run it (installing as secondary ROM)
+
+# MultiROM recognition
+MROM=$(ls /tmp | grep "META-INF")
+
+# Format
+FORMAT() {
     if [ "$TYPE" == "ext4" ]; then
         echo "Found ext4 filesystem. Formatting..."
         mke2fs -T ext4 /dev/block/mmcblk0p16
-    elif [ "$TYPE" == "" ]; then
-        echo "Found f2fs filesystem. Formatting..."
+    elif [ "$TYPE" == "f2fs" ]; then
+        echo "Found F2FS filesystem. Formatting..."
         mkfs.f2fs /dev/block/mmcblk0p16
     else
         # Some recoveries may not have F2FS tools
         echo "No filesystem specified. Formatting as ext4..."
         mke2fs -T ext4 /dev/block/mmcblk0p16
     fi
+    exit 0
+}
+
+# Primary ROM
+MAIN() {
+    echo "Installing in /system"
+    # Recognize FS type
+    FS=$(eval $(blkid /dev/block/mmcblk0p16 | cut -c 68-); echo $TYPE > /tmp/type.txt)
+    TYPE=`cat /tmp/type.txt`
+    FORMAT
+}
+
+# Start
+echo "Checking..."
+if [ "$MROM" == "" ]; then
+    MAIN
 else
-    echo "Installing ROM in MultiROM"
+    echo "Installing in MultiROM environment"
+    exit 0
 fi
-exit 0
