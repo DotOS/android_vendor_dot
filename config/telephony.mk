6651414b8a8aea1c8e2a42c9b6e5b80c73b269cb
razorloves <razorloves@gmail.com>
2016-02-18 23:47:47 -0600

    Revert "cm: add selective based spn (2/2)"
    Appears to be unused in CM13.
    
    This reverts commit 2cfe6b260bf743cbf3091095807b373037581181.
    
    Change-Id: Ia9beee776fd86f516ed9834f92530355a189f326
    
    Conflicts:
    	config/telephony.mk

--- config/telephony.mk
+++ config/telephony.mk
@@ -2,6 +2,12 @@
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
-# Selective SPN list for operator number who has the problem.
-PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+# Telephony packages
+PRODUCT_PACKAGES += \
+    messaging \
+    Stk \
+    CellBroadcastReceiver
+
+# Default ringtone
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=Orion.ogg
