3e10a9ca5982d6140f0cb115484e78440b4bc8fa
André Pinela <sheffzor@gmail.com>
2017-01-22 12:51:01 +0000

    tasks: enhancement to create dotOS ota package
    - modify for DotOS
    - add bacon target
    - don't touch aosp otapackage target
    
    Change-Id: Id243c3099b4914ed3b53efc42c0c7dac9d56dea8
    Signed-off-by: André Pinela <sheffzor@gmail.com>

---
+++ build/tasks/bacon.mk
@@ -0,0 +1,26 @@
+# Copyright (C) 2017 Unlegacy-Android
+# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2021 The dotOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# -----------------------------------------------------------------
+# DotOS OTA update package
+
+DOT_TARGET_PACKAGE := $(PRODUCT_OUT)/$(DOT_VERSION).zip
+
+.PHONY: bacon
+bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
+	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(DOT_TARGET_PACKAGE)
+	$(hide) $(MD5SUM) $(DOT_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(DOT_TARGET_PACKAGE).md5sum
+	@echo "Package Complete: $(DOT_TARGET_PACKAGE)" >&2
