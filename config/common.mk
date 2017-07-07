ViperOS/android_vendor_viper

prebuilt: Add O emojis

config/common.mk
@@ -121,6 +121,10 @@ PRODUCT_COPY_FILES +=  \
     vendor/viper/prebuilt/common/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
 endif
 
+# Android O emojis
+PRODUCT_COPY_FILES += \
+    vendor/viper/prebuilt/common/fonts/NotoColorEmoji.ttf:system/fonts/NotoColorEmoji.ttf
+
 # Required VIPER packages
 PRODUCT_PACKAGES += \
     BluetoothExt \

DotOS/android_vendor_dot

config/common.mk
@@ -96,7 +96,10 @@ endif
 PRODUCT_PACKAGES += \
     bootanimation.zip
 
-# Required CM packages
+# Android O emojis
+PRODUCT_COPY_FILES += \
+    vendor/dot/prebuilt/common/fonts/NotoColorEmoji.ttf:system/fonts/NotoColorEmoji.ttf
+
 PRODUCT_PACKAGES += \
     BluetoothExt \
     CMAudioService \