BlissRoms/platform_vendor_bliss

Bliss: Bringup system-squisher for Nougat (2/2)
Other commit is in build
Based on Cl3kener and FusionJacks Lollipop commits

Signed-off-by: Jackeagle <jackeagle102@gmail.com>

config/common.mk
@@ -72,6 +72,9 @@ ifneq ($(TARGET_BUILD_VARIANT),eng)
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
 endif
 
+# Squisher Location
+SQUISHER_SCRIPT := vendor/bliss/tools/squisher
+
 # Bliss Packages
 -include vendor/bliss/config/bliss_packages.mk
 

DotOS/android_vendor_dot

config/common.mk
@@ -259,6 +259,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
  ro.substratum.verified=true \
  ro.opa.eligible_device=true
 
+# Squisher Location
+SQUISHER_SCRIPT := vendor/dot/tools/squisher
+
 DOT_DISPLAY_VERSION := $(DOT_VERSION)
 
 PRODUCT_PROPERTY_OVERRIDES += \