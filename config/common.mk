54ad47b09e4ad741c84dbda9429d2b9003eae925
B--B <dark.schneider.ds@gmail.com>
2017-07-16 01:51:47 +0200

    Common: build tcmiface
    NOTE: must be tested on all devices

--- config/common.mk
+++ config/common.mk
@@ -140,3 +140,10 @@ ifeq ($(WITH_ROOT),true)
 else
 $(warning Root method is undefined, please use 'WITH_ROOT := true' to define it)
 endif
+
+# TCM (TCP Connection Management)
+PRODUCT_PACKAGES += \
+    tcmiface
+
+PRODUCT_BOOT_JARS += \
+    tcmiface
