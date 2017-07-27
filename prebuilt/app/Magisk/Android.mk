21ff2130aa509bf2c3447185254f84c1a8565bea
Mohan CM <mohanmanjappa@gmail.com>
2017-07-27 09:16:32 +0530

    Update Magiskmanager to Latest-v5.1.1
    Signed-off-by: Mohan CM <mohanmanjappa@gmail.com>

---
+++ prebuilt/app/Magisk/Android.mk
@@ -0,0 +1,14 @@
+# MagiskManager-v5.1.1
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := MagiskManager
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_BUILT_MODULE_STEM := package.apk
+# Make sure the build system doesn't try to resign the APK
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_DEX_PREOPT := false
+LOCAL_SRC_FILES := MagiskManager.apk
+include $(BUILD_PREBUILT)
