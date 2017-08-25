af1a49e035aceba947dfd00773ac11e268be3b45
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-08-25 16:19:30 +0200

    envsetup: port missing fixup_common_out_dir function
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -37,3 +37,21 @@ function mk_timer()
 function mka() {
     m -j "$@"
 }
+
+function fixup_common_out_dir() {
+    common_out_dir=$(get_build_var OUT_DIR)/target/common
+    target_device=$(get_build_var TARGET_DEVICE)
+    if [ ! -z $JDC_FIXUP_COMMON_OUT ]; then
+        if [ -d ${common_out_dir} ] && [ ! -L ${common_out_dir} ]; then
+            mv ${common_out_dir} ${common_out_dir}-${target_device}
+            ln -s ${common_out_dir}-${target_device} ${common_out_dir}
+        else
+            [ -L ${common_out_dir} ] && rm ${common_out_dir}
+            mkdir -p ${common_out_dir}-${target_device}
+            ln -s ${common_out_dir}-${target_device} ${common_out_dir}
+        fi
+    else
+        [ -L ${common_out_dir} ] && rm ${common_out_dir}
+        mkdir -p ${common_out_dir}
+    fi
+}
