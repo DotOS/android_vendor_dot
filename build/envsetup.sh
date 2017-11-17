d41c10669b6d4f9a76aa174295e10a44953a0b45
Mohan CM <mohanmanjappa@gmail.com>
2017-11-17 21:09:10 +0530

    fix zip file path

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -81,7 +81,7 @@ function eat()
 {
     if [ "$OUT" ] ; then
         MODVERSION=$(get_build_var DOT_VERSION)
-        ZIPFILE=dot-$MODVERSION.zip
+        ZIPFILE=$MODVERSION.zip
         ZIPPATH=$OUT/$ZIPFILE
         if [ ! -f $ZIPPATH ] ; then
             echo "Nothing to eat"
