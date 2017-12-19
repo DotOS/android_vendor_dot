352640c86baa4cdad7e81da932ea7e5172c07b36
ganeshi4u <ganeshvarmai4u@gmail.com>
2017-12-19 22:29:40 +0530

    update OTA generator
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>

--- tools/ota_gen.sh
+++ tools/ota_gen.sh
@@ -4,29 +4,47 @@ device=$(echo $TARGET_PRODUCT | cut -d _ -f 2,3)
 echo ".................................."
 echo "....Generating OTA for $device...."
 echo ".................................."
+start_dir=`pwd`
 
-if [ -f .xml ]; then
-	rm .xml
+if [ -f $start_dir/*.xml ]; then
+	rm $start_dir/*.xml
 fi
 
-cp vendor/dot/ota.xml $device.xml
+cp vendor/dot/ota.xml $start_dir/$device.xml
 
-touch vendor/dot/$device.xml
+touch $start_dir/$device.xml
 
-DOT_OTA_MD5=$(cat "$OUT/$(DOT_TARGET_PACKAGE).md5sum" | cut -d ' ' -f 1)
-DOT_OTA_FILESIZE=$(ls -la "$OUT/$(DOT_TARGET_PACKAGE)" | cut -d ' ' -f 5)
+DOT_OTA_MD5=$(cat "$OUT/${DOT_TARGET_PACKAGE}.md5sum" | cut -d ' ' -f 1)
+DOT_OTA_FILESIZE=$(ls -la "$OUT/${DOT_TARGET_PACKAGE}" | cut -d ' ' -f 5)
 DOT_OTA_ROM=dot_"$device"
-DOT_OTA_VERNUM=$(shell date +%Y%m%d)
-DOT_OTA_URL=https://sourceforge.net/projects/dotos-ota/files/$device/$DOT_TARGET_PACKAGE
+DOT_OTA_VERNUM=$(date +%Y%m%d)
+DOT_OTA_URL=https://sourceforge.net/projects/dotos-ota/files/$device/${DOT_TARGET_PACKAGE}
 DOT_HTTP_URL=https://sourceforge.net/projects/dotos-ota/files/$device
 
-sed -i "s/DOT_OTA_ROM/$DOT_OTA_ROM/g" $device.xml
-sed -i "s/DOT_OTA_VERSION/${DOT_VERSION}/g" $device.xml
-sed -i "s/DOT_OTA_VERNUM/$DOT_OTA_VERNUM/g" $device.xml
-sed -i "s|DOT_OTA_URL|$DOT_OTA_URL|g" $device.xml
-sed -i "s|DOT_HTTP_URL|$DOT_HTTP_URL|g" $device.xml
-sed -i "s/DOT_OTA_MD5/DOT_OTA_MD5/g" $device.xml
-sed -i "s/DOT_OTA_FILESIZE/DOT_OTA_FILESIZE/g" $device.xml
+sed -i "s/DOT_OTA_ROM/$DOT_OTA_ROM/g" $start_dir/$device.xml
+sed -i "s/DOT_OTA_VERSION/${DOT_VERSION}/g" $start_dir/$device.xml
+sed -i "s/DOT_OTA_VERNUM/$DOT_OTA_VERNUM/g" $start_dir/$device.xml
+sed -i "s|DOT_OTA_URL|$DOT_OTA_URL|g" $start_dir/$device.xml
+sed -i "s|DOT_HTTP_URL|$DOT_HTTP_URL|g" $start_dir/$device.xml
+sed -i "s/DOT_OTA_MD5/DOT_OTA_MD5/g" $start_dir/$device.xml
+sed -i "s/DOT_OTA_FILESIZE/DOT_OTA_FILESIZE/g" $start_dir/$device.xml
+
+push() {
+        cd ${ANDROID_BUILD_TOP}/ota
+	git reset --hard HEAD
+	git clean -f
+	git pull
+	cp $start_dir/$device.xml ${ANDROID_BUILD_TOP}/ota/$device.xml
+        git add -A
+        git commit -m "$device: weekly update ($(date +'%d/%m/%Y'))"
+        git push --force origin dot-n
+	cd ${ANDROID_BUILD_TOP}
+
+	if [ $? -ne 0 ]; then
+		echo "Failed to push xml!!"
+		cd ${ANDROID_BUILD_TOP}
+	fi
+}
 
 if [ -d ${ANDROID_BUILD_TOP}/ota ]; then
 	push
@@ -35,11 +53,3 @@ else
 	git clone git@github.com:DotOS/services_apps_ota.git ota
 	push
 fi
-
-push () {
-	mv vendor/dot/tools/$device.xml ${ANDROID_BUILD_TOP}/ota/$device.xml
-	cd ${ANDROID_BUILD_TOP}/ota
-	git add -A
-	git commit -m "$device: weekly update ($(date -d "$mdate" +'%d/%m/%Y'))"
-	git push -f -s origin dot-n
-}
