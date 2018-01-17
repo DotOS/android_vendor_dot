AospExtended/platform_vendor_aosp

Provide our custom envsetup.sh
Big thanks to LOS guys

Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

build/envsetup.sh
@@ -1,0 +1,39 @@
+function __print_jdc_functions_help() {
+cat <<EOF
+Additional JDC functions:
+- mka:             Builds using SCHED_BATCH on all processors.
+EOF
+} 
+
+function mk_timer()
+{
+    local start_time=$(date +"%s")
+    $@
+    local ret=$?
+    local end_time=$(date +"%s")
+    local tdiff=$(($end_time-$start_time))
+    local hours=$(($tdiff / 3600 ))
+    local mins=$((($tdiff % 3600) / 60))
+    local secs=$(($tdiff % 60))
+    local ncolors=$(tput colors 2>/dev/null)
+    echo
+    if [ $ret -eq 0 ] ; then
+        echo -n "#### make completed successfully "
+    else
+        echo -n "#### make failed to build some targets "
+    fi
+    if [ $hours -gt 0 ] ; then
+        printf "(%02g:%02g:%02g (hh:mm:ss))" $hours $mins $secs
+    elif [ $mins -gt 0 ] ; then
+        printf "(%02g:%02g (mm:ss))" $mins $secs
+    elif [ $secs -gt 0 ] ; then
+        printf "(%s seconds)" $secs
+    fi
+    echo " ####"
+    echo
+    return $ret
+}
+
+function mka() {
+    m -j "$@"
+}

DotOS/android_vendor_dot

Signed-off-by: Mohan CM <mohanmanjappa@gmail.com>

build/envsetup.sh
@@ -1,0 +1,39 @@
+function __print_dot_functions_help() {
+cat <<EOF
+Additional dotOS functions:
+- mka:             Builds using SCHED_BATCH on all processors.
+EOF
+} 
+
+function mk_timer()
+{
+    local start_time=$(date +"%s")
+    $@
+    local ret=$?
+    local end_time=$(date +"%s")
+    local tdiff=$(($end_time-$start_time))
+    local hours=$(($tdiff / 3600 ))
+    local mins=$((($tdiff % 3600) / 60))
+    local secs=$(($tdiff % 60))
+    local ncolors=$(tput colors 2>/dev/null)
+    echo
+    if [ $ret -eq 0 ] ; then
+        echo -n "#### make completed successfully "
+    else
+        echo -n "#### make failed to build some targets "
+    fi
+    if [ $hours -gt 0 ] ; then
+        printf "(%02g:%02g:%02g (hh:mm:ss))" $hours $mins $secs
+    elif [ $mins -gt 0 ] ; then
+        printf "(%02g:%02g (mm:ss))" $mins $secs
+    elif [ $secs -gt 0 ] ; then
+        printf "(%s seconds)" $secs
+    fi
+    echo " ####"
+    echo
+    return $ret
+}
+
+function mka() {
+    m -j "$@"
+}