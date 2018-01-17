OmniRom-CAF/vendor_aosp

Soong flags: cleanup
Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

build/soong/soong_config.mk
@@ -1,8 +1,9 @@
+# Insert new variables inside the JDC structure
 jdc_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
-	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
-	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
-	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
+	echo '"JDC": {'; \
+	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)

DotOS/android_vendor_dot

Signed-off-by: Mohan CM <mohanmanjappa@gmail.com>

build/soong/soong_config.mk
@@ -1,8 +1,9 @@
+# Insert new variables inside the dotOS structure
 dot_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
-	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
-	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
-	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
+	echo '"dotOS": {'; \
+	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
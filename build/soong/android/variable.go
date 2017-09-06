3ce23b75d8f91635e6c3fd18fccdad7b934faa53
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-09-06 19:16:40 +0200

    Soong flags: cleanup
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>
    Signed-off-by: Mohan CM <mohanmanjappa@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,20 +1,10 @@
 package android
 type Product_variables struct {
-	Has_legacy_camera_hal1 struct {
-		Cflags []string
-	}
-
-	Uses_media_extensions struct {
-		Cflags []string
-	}
-
 	Needs_text_relocations struct {
 		Cppflags []string
 	}
 }
 
 type ProductVariables struct {
-	Has_legacy_camera_hal1  *bool `json:",omitempty"`
-	Uses_media_extensions   *bool `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,8 +1,9 @@
+# Insert new variables inside the dotOS structure
 dot_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
-	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
-	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
-	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
+	echo '"dotOS": {'; \
+	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
