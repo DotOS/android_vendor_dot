ad95b50c0b656f074e1b54b256b2423b51932fa1
ganeshi4u <ganeshvarmai4u@gmail.com>
2018-03-05 17:14:59 +0530

    bring in official and beta tag
    Change-Id: I72cc7965fdc2c3f3558f0d612294d69c2fa7987d
    Signed-off-by: ganeshi4u <ganeshvarmai4u@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -20,6 +20,34 @@ ifndef DOT_BUILD_TYPE
     DOT_BUILD_TYPE := UNOFFICIAL
 endif
 
+ifeq ($(DOT_BETA),true)
+    DOT_BUILD_TYPE := BETA
+endif
+
+ifdef DOT_BUILD_TYPE
+   CURRENT_DEVICE=$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
+   LIST = $(shell curl -s 
+https://raw.githubusercontent.com/DotOS/android_vendor_dot/dot-o/dot.devices)
+   FOUND_DEVICE =  $(filter $(CURRENT_DEVICE), $(LIST))
+    ifeq ($(FOUND_DEVICE),$(CURRENT_DEVICE))
+      IS_OFFICIAL=true
+      DOT_BUILD_TYPE := OFFICIAL
+      
+      #PRODUCT_PACKAGES += \
+      #DotOTA
+    endif
+    ifneq ($(IS_OFFICIAL), true)
+       DOT_BUILD_TYPE := UNOFFICIAL
+       $(error Device is not official "$(FOUND)")
+    endif
+    #PRODUCT_PROPERTY_OVERRIDES += \
+    #    persist.ota.romname=$(TARGET_PRODUCT) \
+    #    persist.ota.version=$(shell date +%Y%m%d) \
+    #    
+persist.ota.manifest=https://raw.githubusercontent.com/DotOS/services_apps_ota/dot-n/$(shell 
+echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3).xml 
+endif
+
 DOT_VERSION := DotOS-$(DOT_MOD_VERSION)-$(shell date -u +%Y%m%d)-$(DOT_BUILD_TYPE)
 
 
