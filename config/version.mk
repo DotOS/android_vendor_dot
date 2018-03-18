a459accb5d6d2b02f055884532c5f3fda983adfa
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-18 22:58:35 +0100

    Move all system props to PRODUCT_GENERIC_PROPERTIES
    For Treble enabled devices, PRODUCT_PROPERTY_OVERRIDES puts props
    added with that in /vendor/build.prop. While that's great for
    device-specific properties, these are global Lineage properties
    which are descriptive of the system side, so move them to the list
    that gets populated into /system/build.prop.
    
    Change-Id: Ie99fdb76f0ad10e59b88183431360148540fb4c0

--- config/version.mk
+++ config/version.mk
@@ -20,8 +20,9 @@ ifndef DOT_BUILD_TYPE
     DOT_BUILD_TYPE := UNOFFICIAL
 endif
 
+CURRENT_DEVICE=$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
+
 ifeq ($(DOT_OFFICIAL), true)
-   CURRENT_DEVICE=$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
    LIST = $(shell curl -s https://raw.githubusercontent.com/DotOS/android_vendor_dot/dot-o/dot.devices)
    FOUND_DEVICE =  $(filter $(CURRENT_DEVICE), $(LIST))
     ifeq ($(FOUND_DEVICE),$(CURRENT_DEVICE))
@@ -34,21 +35,21 @@ ifneq ($(IS_OFFICIAL), true)
        $(error Device is not official "$(FOUND)")
 endif
 
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_GENERIC_PROPERTIES += \
     persist.ota.romname=$(TARGET_PRODUCT) \
     persist.ota.version=$(shell date +%Y%m%d) \
 
 persist.ota.manifest=https://raw.githubusercontent.com/DotOS/services_apps_ota/dot-o/$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3).xml
 endif
 
-DOT_VERSION := dotOS-O-$(DOT_MOD_VERSION)-$(TARGET_DEVICE)-$(DOT_BUILD_TYPE)-$(shell date -u +%Y%m%d)
+DOT_VERSION := dotOS-O-$(DOT_MOD_VERSION)-$(CURRENT_DEVICE)-$(DOT_BUILD_TYPE)-$(shell date -u +%Y%m%d)
 
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_GENERIC_PROPERTIES += \
   ro.dot.version=$(DOT_VERSION) \
   ro.dot.releasetype=$(DOT_BUILD_TYPE) \
   ro.modversion=$(DOT_MOD_VERSION)
 
 DOT_DISPLAY_VERSION := DotOS-$(DOT_MOD_VERSION)-$(DOT_BUILD_TYPE)
 
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_GENERIC_PROPERTIES += \
   ro.dot.display.version=$(DOT_DISPLAY_VERSION)
