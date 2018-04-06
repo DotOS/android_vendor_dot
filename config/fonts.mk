c7ff94064524d587cd87f17818a8edafda4a70bf
ElDainosor <eldainosor@gmail.com>
2018-04-06 06:18:47 +0000

    vendor: Add 3 new fonts
    * This will be used in future plans
    
    Change-Id: Ida9c0aed953397673e671f0e4b8e2e6708f46efb
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -7,4 +7,7 @@ PRODUCT_COPY_FILES += \
     vendor/superior/prebuilt/fonts/gobold/GoboldThinLight.ttf:system/fonts/GoboldThinLight.ttf \
     vendor/superior/prebuilt/fonts/gobold/GoboldThinLight-Italic.ttf:system/fonts/GoboldThinLight-Italic.ttf \
 	vendor/superior/prebuilt/fonts/neoneon/neoneon.ttf:system/fonts/Neoneon-Regular.ttf \
+    vendor/superior/prebuilt/fonts/roadrage/road_rage.ttf:system/fonts/RoadRage-Regular.ttf \
+    vendor/superior/prebuilt/fonts/snowstorm/snowstorm.ttf:system/fonts/Snowstorm-Regular.ttf \
+    vendor/superior/prebuilt/fonts/vcrosd/vcr_osd_mono.ttf:system/fonts/ThemeableFont-Regular.ttf \
     vendor/superior/prebuilt/fonts/Shamshung/Shamshung.ttf:system/fonts/Shamshung.ttf
