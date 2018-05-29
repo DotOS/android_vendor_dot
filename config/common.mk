AOSiP/platform_vendor_aosip

[2/2] Add 'GoogleSans' font family
Change-Id: I15e7760d2b7b30cbfedc0dd3278ea20f924adc25
Signed-off-by: Jagrav Naik <jagravnaik0@gmail.com>

config/common.mk
@@ -110,3 +110,12 @@ include vendor/aosip/config/packages.mk
 
 # Versioning
 include vendor/aosip/config/version.mk
+
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/aosip/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/aosip/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/aosip/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/aosip/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/aosip/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/aosip/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf

DotOS/android_vendor_dot

Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>

config/common.mk
@@ -179,6 +179,16 @@ PRODUCT_COPY_FILES += \
     vendor/dot/prebuilt/common/media/audio/BatteryPlugged.ogg:system/media/audio/ui/BatteryPlugged.ogg \
     vendor/dot/prebuilt/common/media/audio/BatteryPlugged_48k.ogg:system/media/audio/ui/BatteryPlugged_48k.ogg
 
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/dot/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/dot/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/dot/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/dot/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/dot/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/dot/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+
+
 # Recommend using the non debug dexpreopter
 USE_DEX2OAT_DEBUG ?= false
 
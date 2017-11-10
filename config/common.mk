b570b45c9c566aa74d7825dea1aadaae81a5022e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-10 18:36:50 -0200

    [2/2] Add 'GoogleSans' font family
    Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>

--- config/common.mk
+++ config/common.mk
@@ -179,6 +179,16 @@ PRODUCT_COPY_FILES += \
     vendor/dot/prebuilt/common/media/audio/BatteryPlugged.ogg:system/media/audio/ui/BatteryPlugged.ogg \
     vendor/dot/prebuilt/common/media/audio/BatteryPlugged_48k.ogg:system/media/audio/ui/BatteryPlugged_48k.ogg
 
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/dot/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/dot/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/dot/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/dot/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/dot/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/dot/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+
+
 # Recommend using the non debug dexpreopter
 USE_DEX2OAT_DEBUG ?= false
 
