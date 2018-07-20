CarbonROM/android_vendor_carbon

sdclang: Gaurd usage
Change-Id: Ie3e1c8130a7b10e2a721c66fe247a4cf6e8373a8
Signed-off-by: Joe Maples <joe@frap129.org>

config/common.mk
@@ -91,5 +91,9 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/carbon/overlay/common
 # Squisher Location
 SQUISHER_SCRIPT := vendor/carbon/tools/squisher
 
-# include definitions for SDCLANG
-include device/qcom/common/sdclang/sdclang.mk
+# Include SDCLANG definitions if it is requested and available
+ifeq ($(HOST_OS),linux)
+    ifneq ($(wildcard vendor/qcom/sdclang-3.8/),)
+        include vendor/carbon/sdclang/sdclang.mk
+    endif
+endif

DotOS/android_vendor_dot

config/common.mk
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# include definitions for SDCLANG
-# include vendor/dot/sdclang/sdclang.mk
-
 
 PRODUCT_BRAND ?= DotOS
 
@@ -207,4 +204,12 @@ $(call inherit-product, vendor/dot/config/dot_props.mk)
 # Enable ADB authentication
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.adb.secure=1
+endif
+
+	
+# Include SDCLANG definitions if it is requested and available
+ifeq ($(HOST_OS),linux)
+    ifneq ($(wildcard vendor/qcom/sdclang-3.8/),)
+        include vendor/dot/sdclang/sdclang.mk
+    endif
 endif