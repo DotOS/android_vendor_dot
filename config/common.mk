AospExtended/platform_vendor_aosp

Add Pixel's sysconfigs
Signed-off-by: Shubham Singh <coolsks94@gmail.com>

config/common.mk
@@ -66,6 +66,10 @@ PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/lib/libsketchology_native.so:system/lib/libsketchology_native.so \
     vendor/aosp/prebuilt/common/lib64/libsketchology_native.so:system/lib64/libsketchology_native.so
 
+# Pixel sysconfig
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/etc/sysconfig/pixel.xml:system/etc/sysconfig/pixel.xml
+
 # Extra tools
 PRODUCT_PACKAGES += \
     e2fsck \

DotOS/android_vendor_dot

config/common.mk
@@ -109,6 +109,10 @@ PRODUCT_COPY_FILES += \
     vendor/dot/prebuilt/common/lib/libsketchology_native.so:system/lib/libsketchology_native.so \
     vendor/dot/prebuilt/common/lib64/libsketchology_native.so:system/lib64/libsketchology_native.so
 
+# Pixel sysconfig
+PRODUCT_COPY_FILES += \
+    vendor/dot/prebuilt/common/etc/sysconfig/pixel.xml:system/etc/sysconfig/pixel.xml	
+	
 # init.d support
 PRODUCT_COPY_FILES += \
     vendor/dot/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner