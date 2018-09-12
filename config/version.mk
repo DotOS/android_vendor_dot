AospExtended/platform_vendor_aosp

Add ROM's custom fingerprint
Signed-off-by: Shubham Singh <coolsks94@gmail.com>

config/version.mk
@@ -26,8 +26,11 @@ $(call inherit-product-if-exists, vendor/aosp/config/ota.mk)
 
 endif
 
+TARGET_PRODUCT_SHORT := $(subst aosp_,,$(CUSTOM_BUILD))
+
 EXTENDED_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
 EXTENDED_MOD_VERSION := AospExtended-$(EXTENDED_VERSION)-$(EXTENDED_BUILD_DATE)-$(EXTENDED_BUILD_TYPE)
+EXTENDED_FINGERPRINT := AospExtended/$(EXTENDED_VERSION)/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(EXTENDED_BUILD_DATE)
 
 
 PRODUCT_GENERIC_PROPERTIES += \
@@ -38,4 +41,5 @@ PRODUCT_GENERIC_PROPERTIES += \
 EXTENDED_DISPLAY_VERSION := AospExtended-$(EXTENDED_VERSION)-$(EXTENDED_BUILD_TYPE)
 
 PRODUCT_GENERIC_PROPERTIES += \
-  ro.extended.display.version=$(EXTENDED_DISPLAY_VERSION)
+  ro.extended.display.version=$(EXTENDED_DISPLAY_VERSION) \
+  ro.extended.fingerprint=$(EXTENDED_FINGERPRINT)

DotOS/android_vendor_dot

config/version.mk
@@ -42,7 +42,11 @@ PRODUCT_GENERIC_PROPERTIES += \
 persist.ota.manifest=https://raw.githubusercontent.com/DotOS/ota/dot-p/$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3).xml
 endif
 
+TARGET_PRODUCT_SHORT := $(subst dot_,,$(CUSTOM_BUILD))
+
 DOT_VERSION := dotOS-P-$(DOT_MOD_VERSION)-$(CURRENT_DEVICE)-$(DOT_BUILD_TYPE)-$(shell date -u +%Y%m%d)
+
+DOT_FINGERPRINT := dotOS/$(DOT_MOD_VERSION)/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date -u +%Y%m%d)
 
 PRODUCT_GENERIC_PROPERTIES += \
   ro.dot.version=$(DOT_VERSION) \
@@ -52,4 +56,5 @@ PRODUCT_GENERIC_PROPERTIES += \
 DOT_DISPLAY_VERSION := DotOS-$(DOT_MOD_VERSION)-$(DOT_BUILD_TYPE)
 
 PRODUCT_GENERIC_PROPERTIES += \
-  ro.dot.display.version=$(DOT_DISPLAY_VERSION)
+  ro.dot.display.version=$(DOT_DISPLAY_VERSION)\
+  ro.dot.fingerprint=$(EXTENDED_FINGERPRINT)