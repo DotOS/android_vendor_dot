AospExtended/platform_vendor_aosp

Add support to screenshot editor(Markup)
Change-Id: Id1289b70863673b3a6ccd3bf0aa97e68437ee187
Signed-off-by: Shubham Singh <coolsks94@gmail.com>

config/common.mk
@@ -58,7 +58,13 @@ PRODUCT_PACKAGES += \
     ViaBrowser \
     AEXPapers \
     RetroMusicPlayer \
-    WallpaperPickerGoogle
+    WallpaperPickerGoogle \
+    MarkupGoogle
+
+# Markup libs
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/lib/libsketchology_native.so:system/lib/libsketchology_native.so \
+    vendor/aosp/prebuilt/common/lib64/libsketchology_native.so:system/lib64/libsketchology_native.so
 
 # Extra tools
 PRODUCT_PACKAGES += \

DotOS/android_vendor_dot

config/common.mk
@@ -68,7 +68,8 @@ PRODUCT_PACKAGES += \
     Browser \
     DotPapers \
     InterfaceCenter \
-    SystemUpdates
+    SystemUpdates \
+	MarkupGoogle
 
 # Extra tools
 PRODUCT_PACKAGES += \
@@ -103,6 +104,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/dot/config/permissions/backup.xml:system/etc/sysconfig/backup.xml
 
+# Markup libs
+PRODUCT_COPY_FILES += \
+    vendor/dot/prebuilt/common/lib/libsketchology_native.so:system/lib/libsketchology_native.so \
+    vendor/dot/prebuilt/common/lib64/libsketchology_native.so:system/lib64/libsketchology_native.so
 
 # init.d support
 PRODUCT_COPY_FILES += \