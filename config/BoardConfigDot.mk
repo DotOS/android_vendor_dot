1f8312e223ea6da7a4f44551c7551390e121d995
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-18 21:53:24 -0700

    vendor/dot: Dynamically generate kernel headers using lineage generator
    Add a soong vendor plugin for kernel config variables so we can use
    these in go.
    
    Change-Id: Id31f2be8fcc5aba2d965dbe815edaaf1d28279c6

--- config/BoardConfigDot.mk
+++ config/BoardConfigDot.mk
@@ -3,3 +3,5 @@ include vendor/dot/config/BoardConfigKernel.mk
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/dot/config/BoardConfigQcom.mk
 endif
+
+include vendor/dot/config/BoardConfigSoong.mk
