19a05e43f696214af0e217d7ad68981b0f472530
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2018-11-03 21:47:09 +0530

    dot: config: Moved some files from common to their respective make files
    * Created Bootanimation.mk
    * Created packages.mk
    * Added Custom Build Time(Helps when u build more than one times in a day)
    
    Change-Id: I8a6325861600960d55dda2cf149d4d8e998f7563
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

---
+++ config/packages.mk
@@ -0,0 +1,70 @@
+# Copyright (C) 2018 Project dotOS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Custom dotOS packages
+PRODUCT_PACKAGES += \
+    LatinIME \
+    Launcher3 \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    Stk \
+    Recorder \
+    Music \
+    Browser \
+    DotPapers \
+    InterfaceCenter \
+    SystemUpdates \
+    MarkupGoogle \
+    WellbeingPrebuilt
+
+# Extra tools
+PRODUCT_PACKAGES += \
+    e2fsck \
+    mke2fs \
+    tune2fs \
+    mount.exfat \
+    fsck.exfat \
+    mkfs.exfat \
+    mkfs.f2fs \
+    fsck.f2fs \
+    fibmap.f2fs \
+    mkfs.ntfs \
+    fsck.ntfs \
+    mount.ntfs \
+    7z \
+    bzip2 \
+    curl \
+    lib7z \
+    powertop \
+    pigz \
+    tinymix \
+    unrar \
+    unzip \
+    zip \
+	vim \
+    rsync \
+	bash
+
+# Exchange support
+PRODUCT_PACKAGES += \
+    Exchange2
+
+# Needed by some RILs and for some Gapps packages
+PRODUCT_PACKAGES += \
+    librsjni \
+    libprotobuf-cpp-full
+
+# Charger images
+PRODUCT_PACKAGES += \
+    charger_res_images
