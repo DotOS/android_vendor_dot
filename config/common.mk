SuperiorOS/android_vendor_superior

Vendor: Add samsung font
Signed-off-by: Ded_Boi <dhanush911gtr@gmail.com>
Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

config/common.mk
@@ -175,6 +175,9 @@ $(call inherit-product, vendor/superior/config/telephony.mk)
 # Packages
 include vendor/superior/config/packages.mk
 
+# Fonts
+include vendor/superior/config/fonts.mk
+
 # Superior_props
 $(call inherit-product, vendor/superior/config/superior_props.mk)
 

config/fonts.mk
@@ -1,0 +1,3 @@
+# superior Stuff - Copy to System fonts
+PRODUCT_COPY_FILES += \
+    vendor/superior/prebuilt/fonts/Shamshung/Shamshung.ttf:system/fonts/Shamshung.ttf

DotOS/android_vendor_dot

Change-Id: Ibca19da4cedbdb5f89e733cf97fc73588106ac3c

config/common.mk
@@ -100,6 +100,9 @@ include vendor/dot/config/bootanimation.mk
 # Packages
 include vendor/dot/config/packages.mk
 
+# Fonts
+include vendor/dot/config/fonts.mk
+
 ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Disable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0

config/fonts.mk
@@ -1,0 +1,3 @@
+# Dot Stuff - Copy to System fonts
+PRODUCT_COPY_FILES += \
+    vendor/dot/prebuilt/fonts/Shamshung/Shamshung.ttf:system/fonts/Shamshung.ttf