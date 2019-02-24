shakhawat3742/vendor_aosp

[2/2] vendor: add OTA script do delete package cache
after flashing new image delete contents of /data/system/package_cache/
to make sure they are recreated. Outdated cache files are the
reason for the famous 'resource derps'

Change-Id: Iaf1f113a64242254b8fb33bc5b9fdef0c9fdb120

config/common.mk
@@ -23,7 +23,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/aosp/prebuilt/common/bin/sysinit:system/bin/sysinit
+    vendor/aosp/prebuilt/common/bin/sysinit:system/bin/sysinit \
+    vendor/aosp/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
 
 # Init file
 PRODUCT_COPY_FILES += \

DotOS/android_vendor_dot

config/common.mk
@@ -22,6 +22,7 @@ PRODUCT_COPY_FILES += \
     vendor/dot/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/dot/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/dot/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
+    vendor/dot/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
 
 ifeq ($(AB_OTA_UPDATER),true)
 PRODUCT_COPY_FILES += \