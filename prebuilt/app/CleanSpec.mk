60e057c1ea0be4dbbc542e12bdef1e4746e2871a
Mohan Cm <mohanmanjappa@gmail.com>
2019-06-01 09:51:09 +0000

    vendor: use prebuilt SystemUpdates apk for now[Temp] because MaterialComponents have glitches with AospBuild
    Change-Id: Ie5fcc90af58f799d29e6118b6a6e4948b38f4cd4

--- prebuilt/app/CleanSpec.mk
+++ prebuilt/app/CleanSpec.mk
@@ -1,3 +1,4 @@
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/Music)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/Browser)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/DotPapers)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/SystemUpdates)
