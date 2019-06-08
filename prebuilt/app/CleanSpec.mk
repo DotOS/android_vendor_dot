7e4211dafb11379db1bc83b642d68831d78fea6c
Mohan Cm <mohanmanjappa@gmail.com>
2019-06-08 18:49:16 +0000

    vendor: remove prebuilt browser * let's Build from source
    Change-Id: Id14dfe803bbb84b9ca19eb4a043fda026e38adf3
    Signed-off-by: Mohan Cm <mohanmanjappa@gmail.com>

--- prebuilt/app/CleanSpec.mk
+++ prebuilt/app/CleanSpec.mk
@@ -1,3 +1,2 @@
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/Music)
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/Browser)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/DotPapers)
