66c2e0989ff097691af80a0f14e68fdbbf1f9a9b
Jan Altensen <info@stricted.net>
2020-10-17 18:10:26 +0200

    soong: add TARGET_HAS_MEMFD_BACKPORT conditional
    Change-Id: I71b3a86c96bfe53e93e99c071bc8f3a8c56c77b7

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -33,6 +33,7 @@ SOONG_CONFIG_dotGlobalVars += \
     bootloader_message_offset \
     camera_needs_client_info \
     has_legacy_camera_hal1 \
+    has_memfd_backport \
     ignores_ftp_pptp_conntrack_failure \
     needs_netd_direct_connect_rule \
     target_init_vendor_lib \
@@ -67,6 +68,7 @@ endif
 # Soong bool variables
 SOONG_CONFIG_dotGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_dotGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
+SOONG_CONFIG_dotGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_dotGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_dotGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_dotGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
