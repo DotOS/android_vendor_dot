34ced7d352ba283f32c6e99331718f52395d2526
Dyneteve <dyneteve@hentaios.com>
2020-10-26 17:48:57 +0900

    vendor: Import Kai and Victor fonts for theme picker
    Change-Id: Iff9782578f7ec750ca892c7be78d0f0d2752ac39

--- config/fonts.mk
+++ config/fonts.mk
@@ -8,7 +8,25 @@ $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/zilla-slab/fonts.mk)
 
+# Karla family
+PRODUCT_PACKAGES += \
+    Karla-Regular.ttf
+
+# Fraunces family
+PRODUCT_PACKAGES += \
+    Fraunces-SemiBold.ttf
+
+# BigShouldersText family
+PRODUCT_PACKAGES += \
+    BigShouldersText-Bold.ttf \
+    BigShouldersText-ExtraBold.ttf
+
 LOCAL_PATH := vendor/dot/fonts
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts)
 PRODUCT_COPY_FILES += \
     vendor/dot/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
+# Customization overlays
+PRODUCT_PACKAGES += \
+    FontKaiOverlay \
+    FontVictorOverlay
