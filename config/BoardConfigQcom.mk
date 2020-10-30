f03a8a66cb13993f1520750dce5880225dafebfa
LuK1337 <priv.luk@gmail.com>
2020-10-30 13:32:49 +0100

    vendor/dot: Add data-ipa-cfg-mgr to soong namespace in BoardConfigQcom
    Before:
      $ breakfast capricorn && m ipacm
      FAILED: ninja: unknown target 'ipacm', did you mean 'lpadd'?
    
    After:
      $ breakfast capricorn && m ipacm
      * happily compiles *
    
    Change-Id: Ib68978d4f287dcdea45fc21382af05b1890caca4

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -106,3 +106,8 @@ PRODUCT_SOONG_NAMESPACES += $(QCOM_SOONG_NAMESPACE)
 ifneq ($(USE_DEVICE_SPECIFIC_DATASERVICES),true)
     PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/dataservices
 endif
+
+# Add data-ipa-cfg-mgr to PRODUCT_SOONG_NAMESPACES if needed
+ifneq ($(USE_DEVICE_SPECIFIC_DATA_IPA_CFG_MGR),true)
+    PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/data-ipa-cfg-mgr
+endif
