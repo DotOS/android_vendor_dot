62a5e1120af9621c06df7bbcf4fe09fcaeaebd43
Mohan Cm <mohanmanjappa@gmail.com>
2020-12-26 16:39:50 +0000

    vendor/dot : font - remove prebuilt Google fonts
    - fetch from external/google-fonts
    
    Signed-off-by: Mohan C M <mohanmanjappa@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -1,4 +1,13 @@
 # Fonts
+
+# Get google fonts from external
+$(call inherit-product-if-exists, external/google-fonts/arbutus-slab/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/arvo/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/barlow/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/zilla-slab/fonts.mk)
+
 LOCAL_PATH := vendor/dot/fonts
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts)
 PRODUCT_COPY_FILES += \
