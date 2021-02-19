0e6440133bf7abecf7fcfd3d85e44821f14f192e
xstefen <x@stefen.me>
2021-02-19 14:33:56 -0600

    dot: inherit vendor/extra if exists
    Change-Id: Ibe59c46131a57e5af9cc4dc85513908be5bb930d

--- config/common.mk
+++ config/common.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Allow vendor/extra to override any property by setting it first
+$(call inherit-product-if-exists, vendor/extra/product.mk)
+
 PRODUCT_BRAND ?= dotOS
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
