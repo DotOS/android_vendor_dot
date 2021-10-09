4a9a6a73acd94c98c1de4b4ca32ac18d44a88344
Danny Lin <danny@kdrag0n.dev>
2021-10-09 06:24:31 -0700

    product: Enable one-handed mode
    There's nothing device-specific about this feature, so enable it for
    everyone.
    
    Change-Id: I4fcc63c0fbbc07fc9134d0195dd47de04d96bccb

--- config/dot_props.mk
+++ config/dot_props.mk
@@ -34,3 +34,8 @@ PRODUCT_GENERIC_PROPERTIES += \
 # on supported devices with Deep Press input classifier HALs and models
 PRODUCT_PRODUCT_PROPERTIES += \
 	ro.input.video_enabled=false
+
+# Enable one-handed mode
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.support_one_handed_mode=true
+
