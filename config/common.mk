5690ff16a80f10431d568fe4ab1cab684b771178
jhenrique09 <jhsv09@gmail.com>
2021-10-10 15:03:08 -0300

    [REVERTME] Don't enforce privapp-permissions whitelist
    Change-Id: Id9da8531f4d62885642a36b89f11170d80688b5f

--- config/common.mk
+++ config/common.mk
@@ -81,8 +81,8 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/Vendor_045e_Product_0719.kl
 
 # Enforce privapp-permissions whitelist
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.control_privapp_permissions=enforce
+#PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+#    ro.control_privapp_permissions=enforce
 
 # Don't compile SystemUITests
 # EXCLUDE_SYSTEMUI_TESTS := true
