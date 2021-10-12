b7f7758dfc6ad2c6f8be8d43029885bd2b7ae22f
TheScarastic <warabhishek@gmail.com>
2021-10-12 06:09:57 +0000

    Rename fod to udfps
     * now flag is changed to TARGET_SURFACEFLINGER_UDFPS_LIB from TARGET_SURFACEFLINGER_FOD_LIB
    
    Change-Id: I499b31d00a053b865ceaa3b34a9762f980bbf14e

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -38,7 +38,7 @@ SOONG_CONFIG_dotGlobalVars += \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_process_sdk_version_override \
-    target_surfaceflinger_fod_lib \
+    target_surfaceflinger_udfps_lib \
     disable_postrender_cleanup \
     uses_camera_parameter_lib
 
@@ -83,7 +83,7 @@ SOONG_CONFIG_dotQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVI
 BOOTLOADER_MESSAGE_OFFSET ?= 0
 TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY ?= libcamera_parameters
-TARGET_SURFACEFLINGER_FOD_LIB ?= surfaceflinger_fod_lib
+TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 
 # Soong value variables
@@ -93,7 +93,7 @@ SOONG_CONFIG_dotGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_dotGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_dotGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_dotGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
-SOONG_CONFIG_dotGlobalVars_target_surfaceflinger_fod_lib := $(TARGET_SURFACEFLINGER_FOD_LIB)
+SOONG_CONFIG_dotGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
 SOONG_CONFIG_dotGlobalVars_uses_camera_parameter_lib := $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)
 
 # et Qcom Soong Namespace for UM platforms Only
